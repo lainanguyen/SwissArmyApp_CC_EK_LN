1f9ffdaed29f08881fb3df2b0fa9f72d5b2acb11
ElizavKim <elizav.kim@gmail.com>
2023-04-08 17:27:41 -0400

    added files from old repo

---
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/LandingPage.java
@@ -0,0 +1,14 @@
+package com.example.swissarmyapp;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class LandingPage {
+
+
+}
