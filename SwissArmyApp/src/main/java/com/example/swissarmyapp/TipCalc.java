96fa278cb3cac2bbc8ca576e606bec11b101740f
lilcircle <cc926@cairn.edu>
2023-04-23 21:34:47 -0400

    added function and variables to tipcalc controller

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/TipCalc.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/TipCalc.java
@@ -5,8 +5,30 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
 public class TipCalc {
-    
+    @FXML
+    private TextField theInput;
+   @FXML
+   private Label theValue;
+
+   @FXML
+   private void calcTip() {
+       double inputAmount = Double.parseDouble(theInput.getText());
+       double totalAmount = inputAmount * 0.2;
+       theValue.setText(String.format("%.2f",totalAmount));
+   }
+
+   @FXML
+    private void toReset(){
+       theInput.clear();
+       theValue.setText("");
+    }
+
+
+
+
 }
