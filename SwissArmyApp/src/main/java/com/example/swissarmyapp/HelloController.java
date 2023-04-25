94f391eff391ea4263fc89a64b4e16af6e3e0fa3
ElizavKim <elizav.kim@gmail.com>
2023-04-25 00:03:26 -0400

    Update HelloController.java

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/HelloController.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/HelloController.java
@@ -14,19 +14,19 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
-//    @FXML
-//    protected void onHelloButtonClick() {
-//        welcomeText.setText("Welcome to JavaFX Application!");
-//    }
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
 
 // switches to madlibs scene to test if it's working
-    @FXML
-    public void onHelloButtonClick(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Madlibs.class.getResource("madlibs.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setTitle("MadLibs");
-        stage.setScene(scene);
-        stage.show();
-    }
+//    @FXML
+//    public void onHelloButtonClick(ActionEvent event) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(Madlibs.class.getResource("madlibs.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+//        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//        stage.setTitle("MadLibs");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
 }
