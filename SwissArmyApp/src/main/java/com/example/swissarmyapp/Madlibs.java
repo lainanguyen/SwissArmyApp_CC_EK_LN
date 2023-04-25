e81ee2ec35180d09e683b4f18d7501bb31be4935
ElizavKim <elizav.kim@gmail.com>
2023-04-24 20:35:59 -0400

    added to madlibs scene and controller

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/Madlibs.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/Madlibs.java
@@ -1,17 +1,35 @@
 package com.example.swissarmyapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 
 public class Madlibs {
     @FXML
-    protected void chooseStory1() {
-    }
+    private TextField story1Noun;
+    @FXML
+    private TextField story1MU;
+    @FXML
+    private TextField story1Place;
+    @FXML
+    private TextField story1PTV;
+    @FXML
+    private TextField story1Adverb;
+    @FXML
+    private Label story1;
 
     @FXML
-    protected void chooseStory2() {
+    protected void generateStory1() {
+        story1.setText("I shall be telling this with a " + story1Noun +
+                "\nSomewhere " + story1MU + " and " + story1MU + " hence: \n" +
+                "Two roads diverged in a " + story1Place + ", and I - \n" +
+                "I took the one less " + story1PTV + " by,\n" +
+                "And that has made " + story1Adverb + " the difference.");
     }
-
+    @FXML
+    protected void generateStory2() {
+    }
     @FXML
     protected void backToActivities() {
     }
