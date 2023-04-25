0ee2df26788ce3b5d5516e241274b00de5288a0e
ElizavKim <elizav.kim@gmail.com>
2023-04-25 00:02:30 -0400

    added second madlibs story

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/Madlibs.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/Madlibs.java
@@ -7,7 +7,6 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,10 +15,6 @@ import java.io.IOException;
 public class Madlibs {
     @FXML
     private TextField story1Noun;
-//    @FXML
-//    private Text story1Noun;
-//    String noun = story1Noun.getText();
-
     @FXML
     private TextField story1MU;
     @FXML
@@ -32,6 +27,19 @@ public class Madlibs {
     private Label story1;
 
     @FXML
+    private TextField story2Noun;
+    @FXML
+    private TextField story2Noun2;
+    @FXML
+    private TextField story2Number;
+    @FXML
+    private TextField story2Adjective;
+    @FXML
+    private TextField story2Noun3;
+    @FXML
+    private Label story2;
+
+    @FXML
     protected void generateStory1() {
         story1.setText("I shall be telling this with a " + story1Noun.getText() +
                     "\nSomewhere " + story1MU.getText() + " and " + story1MU.getText() + " hence: \n" +
@@ -41,6 +49,10 @@ public class Madlibs {
     }
     @FXML
     protected void generateStory2() {
+        story2.setText("Tony's Burritos serves, as its name suggests, burritos - thick as your " + story2Noun.getText() +
+                ", messy as \nheck, crisped in the oven to add some structure and filled with all the usual suspects - \nrice and beans, "
+                + story2Noun2.getText() + " and sour cream, avocado for $" + story2Number.getText() + ". The pork al pastor gets a \nlittle lost in the noise of " + story2Adjective.getText() +
+                " starches, but the " + story2Noun3.getText() + " version is solid if you're \ncomfortable ordering a " + story2Noun3.getText() + " burrito from a beer store.");
     }
 
     @FXML
