21df8c99e8cda4386b33850a80e48de68633dfe8
lilcircle <cc926@cairn.edu>
2023-04-27 16:12:10 -0400

    added functionality to landingpage button

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/ActivityPage.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/ActivityPage.java
@@ -9,5 +9,15 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import java.io.IOException;
 public class ActivityPage {
+    @FXML
+    public void theNextButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ActivityPage.class.getResource("activitypage.fxml")); //change to file name connecting to
+        Scene scene = new Scene(fxmlLoader.load(), 1206, 790); //size of the window
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("Activities");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
 
--- SwissArmyApp/src/main/java/com/example/swissarmyapp/LandingPage.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/LandingPage.java
@@ -4,11 +4,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LandingPage {
-
+    @FXML
+    public void theNextButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ActivityPage.class.getResource("activitypage.fxml")); //change to file name connecting to
+        Scene scene = new Scene(fxmlLoader.load(), 1206, 790); //size of the window
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("Activities");
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
