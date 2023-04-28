c75e014ad375dd32babcf5529d376baa6aae7cd9
ElizavKim <elizav.kim@gmail.com>
2023-04-28 17:38:28 -0400

    created color change setting

--- SwissArmyApp/src/main/java/com/example/swissarmyapp/Settings.java
+++ SwissArmyApp/src/main/java/com/example/swissarmyapp/Settings.java
@@ -3,7 +3,6 @@ package com.example.swissarmyapp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ToggleButton;
@@ -13,17 +12,43 @@ import java.io.IOException;
 
 public class Settings {
     @FXML
-    public void backToActivities(ActionEvent event) throws IOException {
+    private ToggleButton colorToggle;
+
+    @FXML
+    boolean redMode;
+
+    @FXML
+    public void changeColor(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Madlibs.class.getResource("setting1.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1206, 790);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        if (redMode == false) {
+            scene.getStylesheets().add(HelloApplication.class.getResource("/colorchange.css").toExternalForm());
+        } else {
+            scene.getStylesheets().remove(HelloApplication.class.getResource("/colorchange.css").toExternalForm());
+        }
+        stage.setTitle("Settings");
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void backToActivities (ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LandingPage.class.getResource("activitypage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1206, 790);
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        if (redMode == false) {
+            scene.getStylesheets().add(HelloApplication.class.getResource("/colorchange.css").toExternalForm());
+        } else {
+            scene.getStylesheets().remove(HelloApplication.class.getResource("/colorchange.css").toExternalForm());
+        }
+
         stage.setTitle("ActivityPage");
         stage.setScene(scene);
         stage.show();
     }
+
 }
-
-
 
 
 
